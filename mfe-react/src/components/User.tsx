bc2caf2e6378b0f81ab3842da2f683c29bede477
Gus <42622434+gus-code@users.noreply.github.com>
2023-10-31 02:56:27 +0000

    ACT5 - gustavo - add conditional rendering

--- mfe-react/src/components/User.tsx
+++ mfe-react/src/components/User.tsx
@@ -5,27 +5,31 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
 function User({ name, email, phone, address }) {
-  const containerStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  };
+    const containerStyle = {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+    };
 
-  return (
-    <Container maxWidth="sm" style={containerStyle}>
-      <Avatar sx={{ width: 100, height: 100 }}>GG</Avatar>
+    const displayInfo = true;
 
-      <Typography variant="h3">{name}</Typography>
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography variant="h5">Information</Typography>
-          <Typography variant="h6" component="div">Email: {email}</Typography>
-          <Typography variant="h6" component="div">Phone: {phone}</Typography>
-          <Typography variant="h6" component="div">Address: {address}</Typography>
-        </CardContent>
-      </Card>
-    </Container>
-  )
+    return (
+        <Container maxWidth="sm" style={containerStyle}>
+            <Avatar sx={{ width: 100, height: 100 }}>GG</Avatar>
+
+            {displayInfo && <>
+                <Typography variant="h3">{name}</Typography>
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                        <Typography variant="h5">Information</Typography>
+                        <Typography variant="h6" component="div">Email: {email}</Typography>
+                        <Typography variant="h6" component="div">Phone: {phone}</Typography>
+                        <Typography variant="h6" component="div">Address: {address}</Typography>
+                    </CardContent>
+                </Card>
+            </>}
+        </Container>
+    )
 }
 
 export default User

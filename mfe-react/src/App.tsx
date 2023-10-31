0023c5d2716e7a10347e17236137b3b8dc328f8f
Gus <42622434+gus-code@users.noreply.github.com>
2023-10-31 02:55:28 +0000

    ACT4 - gustavo - add user component

--- mfe-react/src/App.tsx
+++ mfe-react/src/App.tsx
@@ -1,34 +1,13 @@
-import Avatar from '@mui/material/Avatar';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-
+import User from './components/User'
 import './App.css'
 
 function App() {
   const userProfile = { name: 'Gustavo Garcia', email: 'gustavo@example.com', phone: 81123456789, address: '21 Main Street, Monterrey Nuevo Leon, CP65343' };
 
-  const containerStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  };
-
   return (
-    <Container maxWidth="sm" style={containerStyle}>
-      <Avatar sx={{ width: 100, height: 100 }}>GG</Avatar>
-
-      <Typography variant="h3">{userProfile.name}</Typography>
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography variant="h5">Information</Typography>
-          <Typography variant="h6" component="div">Email: {userProfile.email}</Typography>
-          <Typography variant="h6" component="div">Phone: {userProfile.phone}</Typography>
-          <Typography variant="h6" component="div">Address: {userProfile.address}</Typography>
-        </CardContent>
-      </Card>
-    </Container>
+    <>
+      <User {...userProfile} />
+    </>
   )
 }
 
---
+++ mfe-react/src/components/User.tsx
@@ -0,0 +1,31 @@
+import Avatar from '@mui/material/Avatar';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+function User({ name, email, phone, address }) {
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  };
+
+  return (
+    <Container maxWidth="sm" style={containerStyle}>
+      <Avatar sx={{ width: 100, height: 100 }}>GG</Avatar>
+
+      <Typography variant="h3">{name}</Typography>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h5">Information</Typography>
+          <Typography variant="h6" component="div">Email: {email}</Typography>
+          <Typography variant="h6" component="div">Phone: {phone}</Typography>
+          <Typography variant="h6" component="div">Address: {address}</Typography>
+        </CardContent>
+      </Card>
+    </Container>
+  )
+}
+
+export default User

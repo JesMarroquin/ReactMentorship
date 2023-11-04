17797218ae5cb4e95a8d94839fee857c4ae9090f
bapabawka <mcalmore@gmail.com>
2023-11-04 00:45:18 +0000

    ACT5 - mariel_c - Adding User conditional render

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,26 +1,26 @@
 import User from './components/User'
+import UserProfile from './interfaces/userProfile'
 import './App.css'
 
-interface userProfile {
-  name: string;
-  email: string;
-  phone: number;
-  address: string;
-}
-
 function App() {
-  const userProfile: userProfile = {
+  const userProfile: UserProfile = {
     name: 'Mariel Calderon',
     email: 'mcalderon@sample.com',
     phone: 8113456233,
     address: '21 Main Street, Monterrey Nuevo Leon, CP65343'
   }
 
-  return (
-    <>
-      <User profile={userProfile}/>
-    </>
-  )
+  const isUserProfileVisible: boolean = false
+
+  if (isUserProfileVisible) {
+    return (
+      <>
+        <User name={userProfile.name} email={userProfile.email} phone={userProfile.phone} address={userProfile.address}/>
+      </>
+    )
+  } else {
+    return <h1 style={{color:'black'}}>Adventure Awaits!</h1>
+  }
 }
 
 export default App
--- react-app/src/components/User.tsx
+++ react-app/src/components/User.tsx
@@ -1,18 +1,19 @@
 import { Avatar, Paper, Typography } from '@mui/material'
+import UserProfile from '../interfaces/userProfile'
 
-function User(props: any) {
+function User(props: UserProfile) {
   return (
     <>
       <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
         <Avatar style={{width:130, height:130, marginBottom:25, backgroundColor:'grey'}}>
           <Typography variant="h2">MC</Typography>
         </Avatar>
-        <Typography variant="h4" style={{color:'black', marginBottom:15}}>{props.profile.name}</Typography>
+        <Typography variant="h4" style={{color:'black', marginBottom:15}}>{props.name}</Typography>
         <Paper style={{width:'100%', padding:30}}>
           <Typography variant="h5" style={{marginBottom:15}}>Information</Typography>
-          <Typography variant="h6">Email: {props.profile.email}</Typography>
-          <Typography variant="h6">Phone: {props.profile.phone}</Typography>
-          <Typography variant="h6">Address: {props.profile.address}</Typography>
+          <Typography variant="h6">Email: {props.email}</Typography>
+          <Typography variant="h6">Phone: {props.phone}</Typography>
+          <Typography variant="h6">Address: {props.address}</Typography>
         </Paper>
       </div>
     </>

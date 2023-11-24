7a39705ddc39d73707781e95697020d24e2eeb74
NiverMtz <nivermartinezmed@gmail.com>
2023-11-24 16:36:43 -0600

    ACT8 - NiverMtz -  useState and useEffect to set users array

--- app-react/src/App.tsx
+++ app-react/src/App.tsx
@@ -2,14 +2,19 @@ import { Typography } from "@mui/material";
 import "./App.css";
 import { users } from "./users";
 import UserList from "./components/UserList";
+import { useEffect, useState } from "react";
+import User from "./components/User";
 
 function App() {
+  // useState
+  const [usersList, setUsersList] = useState<User[]>([]);
   const isVisible = true;
 
+  useEffect(() => setUsersList(users), []);
   return (
     <>
       {isVisible ? (
-        <UserList users={users} />
+        <UserList users={usersList} />
       ) : (
         <Typography variant="h1">Not found...</Typography>
       )}
